7b2fe39b698d8f2b0e997b36718764dd5f698f43
AlexMejiaArias <apa.mejia@yavirac.edu.ec>
2020-08-04 20:39:59 -0500

    deber algoritmos de ordenamiento

---
+++ Ordenamientos/OrdenamientoShell.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Collections.Generic;
+
+namespace Ordenamientos
+{
+    internal class OrdenamientoShell
+    {
+        internal int[] ShellShort(int numero1, int numero2, int numero3, int numero4, int numero5, int numero6, int numero7, int numero8, int numero9)
+        {
+
+
+            var resultado = new[] { numero1, numero2, numero3, numero4, numero5, numero6, numero7, numero8, numero9 };
+
+            /*Metodo de ordenamiento ShellShort=> Consiste en comparar los elementos con saltos de mayor tamano,
+             pero con incrementos decrecientes, asi los elementos quedaran ordenados mas rapidamente*/
+
+            int sizeArray = resultado.Length;//tama;o del array
+            int inter = sizeArray / 2; // dividimos el array a la mitad y asi obtenemos el intervalo de salto 
+            int temp;
+
+            while (inter > 0) // miestras el salto o intervao de sakto sea mayor a 0
+            {
+                for (int i = 0; i + inter < sizeArray; i++)
+                {
+                    int j = i + inter;
+                    temp = resultado[j];
+
+                    while (j - inter >= 0 && temp < resultado[j - inter])
+                    {
+                        resultado[j] = resultado[j - inter];
+                        j = j - inter;
+                    }
+
+                    resultado[j] = temp;
+                }
+
+                inter = inter / 2;
+            }
+
+
+
+
+            return resultado;
+
+
+
+        }
+
+        internal int[] bubbleSort(int numero1, int numero2, int numero3, int numero4, int numero5, int numero6, int numero7, int numero8, int numero9)
+        {
+
+            var resultado = new[] { numero1, numero2, numero3, numero4, numero5, numero6, numero7, numero8, numero9 };
+            int pivote = 0;
+
+            for (int i = 0; i < resultado.Length; i++)
+            {
+                for (int j = 0; j < resultado.Length - 1; j++)
+                {
+                    if (resultado[j] > resultado[j + 1])
+                    {
+                        pivote = resultado[j + 1];
+                        resultado[j + 1] = resultado[j];
+                        resultado[j] = pivote;
+                    }
+                }
+            }
+
+
+            return resultado;
+
+
+
+
+        }
+
+
+        internal int[] quickSort(int numero1, int numero2, int numero3)
+        {
+            //Declaramos una variable y gradamos en un array los valores que recibimos
+            var resultado = new[] { numero1, numero2, numero3};
+
+            int left = 0;
+            int right =  resultado.Length-1;
+            int i = left;
+            int j = right;
+
+            var pivot = resultado[(left + right) / 2];
+
+            while (i <= j)
+            {
+                while (resultado[i] < pivot)
+                    i++;
+
+                while (resultado[j] > pivot)
+                    j--;
+
+                if (i <= j)
+                {
+                    var tmp = resultado[i];
+                    resultado[i] = resultado[j];
+                    resultado[j] = tmp;
+
+                    i++;
+                    j--;
+                }
+            }
+
+
+
+            return resultado;
+        }
+
+        internal int[] heapSort(int numero1, int numero2, int numero3)
+        {
+            //Declaramos una variable y gradamos en un array los valores que recibimos
+            var resultado = new[] { numero1, numero2, numero3 };
+
+          
+
+            return resultado;
+        }
+    }
+
+
+}
---
+++ Ordenamientos/Ordenamientos.Test/OrdenamientoTest.cs
@@ -0,0 +1,140 @@
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ordenamientos
+{
+    [TestClass]
+    public class OrdenamientoTest
+    {
+        [TestMethod]
+        public void shellShort()
+        {
+            // Triple A => Arrange, act, assert
+            var numero1 = 2;
+            var numero2 = 8;
+            var numero3 = 5;
+            var numero4 = 25;
+            var numero5 = 10;
+            var numero6 = 80;
+            var numero7 = 20;
+            var numero8 = 9;
+            var numero9 = 60;
+
+            var esperado1 =  2;
+            var esperado2=  5;
+            var esperado3 =  8;
+            var esperado4 =  9;
+            var esperado5=  10;
+            var esperado6 =  20;
+            var esperado7 =  25;
+            var esperado8=  60;
+            var esperado9 =  80;
+
+        OrdenamientoShell ordenamientoshell = new OrdenamientoShell();
+        int [] esperados = ordenamientoshell.ShellShort(numero1,numero2,numero3,numero4,numero5,numero6,numero7,numero8,numero9);
+
+
+        Assert.AreEqual(esperado1,esperados[0]);
+        Assert.AreEqual(esperado2,esperados[1]);
+        Assert.AreEqual(esperado3,esperados[2]);
+        Assert.AreEqual(esperado4,esperados[3]);
+        Assert.AreEqual(esperado5,esperados[4]);
+        Assert.AreEqual(esperado6,esperados[5]);
+        Assert.AreEqual(esperado7,esperados[6]);
+        Assert.AreEqual(esperado8,esperados[7]);
+        Assert.AreEqual(esperado9,esperados[8]);
+        
+
+            
+        }
+
+
+         [TestMethod]
+        public void bubbleSort()
+        {
+            // Triple A => Arrange, act, assert
+            var numero1 = 2;
+            var numero2 = 8;
+            var numero3 = 5;
+            var numero4 = 25;
+            var numero5 = 10;
+            var numero6 = 80;
+            var numero7 = 20;
+            var numero8 = 9;
+            var numero9 = 60;
+
+            var esperado1 =  2;
+            var esperado2=  5;
+            var esperado3 =  8;
+            var esperado4 =  9;
+            var esperado5=  10;
+            var esperado6 =  20;
+            var esperado7 =  25;
+            var esperado8=  60;
+            var esperado9 =  80;
+
+        OrdenamientoShell ordenamientoshell = new OrdenamientoShell();
+        int [] esperados = ordenamientoshell.bubbleSort(numero1,numero2,numero3,numero4,numero5,numero6,numero7,numero8,numero9);
+
+         Assert.AreEqual(esperado1,esperados[0]);
+        Assert.AreEqual(esperado2,esperados[1]);
+        Assert.AreEqual(esperado3,esperados[2]);
+        Assert.AreEqual(esperado4,esperados[3]);
+        Assert.AreEqual(esperado5,esperados[4]);
+        Assert.AreEqual(esperado6,esperados[5]);
+        Assert.AreEqual(esperado7,esperados[6]);
+        Assert.AreEqual(esperado8,esperados[7]);
+        Assert.AreEqual(esperado9,esperados[8]);
+
+            
+        }
+
+
+        [TestMethod]
+        public void quickSort()
+        {
+            // Triple A => Arrange, act, assert
+            var numero1 = 2;
+            var numero2 = 8;
+            var numero3 = 5;
+       
+
+            var esperado1 =  2;
+            var esperado2=  5;
+            var esperado3 =  8;
+     
+
+        OrdenamientoShell ordenamientoshell = new OrdenamientoShell();
+       
+        int [] esperados = ordenamientoshell.quickSort(numero1,numero2,numero3);
+
+        Assert.AreEqual(esperado1,esperados[0]);
+        Assert.AreEqual(esperado2,esperados[1]);
+        Assert.AreEqual(esperado3,esperados[2]);
+      
+            
+        }
+
+         [TestMethod]
+        public void heapSort()
+        {
+            // Triple A => Arrange, act, assert
+            var numero1 = 50;
+            var numero2 = 20;
+            var numero3 = 5;
+
+            var esperado1 =  5;
+            var esperado2=  20;
+            var esperado3 =  50;
+
+        OrdenamientoShell ordenamientoshell = new OrdenamientoShell();
+        int [] esperados = ordenamientoshell.heapSort(numero1,numero2,numero3);
+
+        Assert.AreEqual(esperado1,esperados[0]);
+        Assert.AreEqual(esperado2,esperados[1]);
+        Assert.AreEqual(esperado3,esperados[2]);
+
+            
+        }
+    }
+}
